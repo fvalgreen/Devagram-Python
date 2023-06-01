148881f60c2c58f5bebec093ba1def030c511aee
fvalgreen <valverde130495@gmail.com>
2023-06-01 13:57:14 -0300

    Configurações para o heroku

---
+++ Server.py
@@ -0,0 +1,11 @@
+import uvicorn
+from decouple import config
+
+if __name__ == "__main__":
+    PORT = config("PORT")
+    if not PORT:
+        PORT = 5000
+
+    PORT = int(PORT)
+
+    uvicorn.run('Main:app', host="0.0.0.0", port=PORT, reload=True)
