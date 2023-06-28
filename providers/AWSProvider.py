bcd3d01eac7ecdc2613c15546cf017f91fe6ff37
fvalgreen <valverde130495@gmail.com>
2023-06-28 11:41:46 -0300

    corrigindo envs

--- providers/AWSProvider.py
+++ providers/AWSProvider.py
@@ -9,7 +9,7 @@ class AWSProvider:
     def upload_arquivo_s3(caminho_para_salvar, caminho_do_arquivo, bucket='devagram-python-fvalgreen'):
         s3_client = boto3.client(
             's3',
-            aws_access_key_id=config('AWS_ACCESS_KEY'),
+            AWS_ACCESS_KEY_ID_id=config('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY')
         )
 
